f0a12837b16ac5b18d2455d8ae2e54d7e2506b62
thi-bo <thibauthoundjo23@email.com>
2024-05-29 15:01:46 +0200

    end for learning app

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,7 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
-
-
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard &Thibaut',
+    default: 'Acme Dashboard Thibaut',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-mauve.vercel.app/'),
+};
 export default function RootLayout({
   children,
 }: {
